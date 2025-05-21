c9b4cfbc1a83d79dd110c47578cae65610e29cc0
Dhanyaja <dhanyaja2003@gmail.com>
2025-05-21 18:32:55 +0530

    final

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -10,10 +10,10 @@ const Campus = () => {
   return (
     <div className='campus'>
         <div className="gallery">
-            <img src={gallery_1} alt="gallery" />
-            <img src={gallery_2} alt="gallery" />
-            <img src={gallery_3} alt="gallery" />
-            <img src={gallery_4} alt="gallery" />
+            <img src={gallery_1} alt="" />
+            <img src={gallery_2} alt="" />
+            <img src={gallery_3} alt="" />
+            <img src={gallery_4} alt="" />
         </div>
         <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
       
